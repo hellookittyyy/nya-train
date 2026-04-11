30b3a37b37de32b39996525cd4ed4f7049d132ce
Kateryna <Katya.shapovalova3012@gmail.com>
2026-04-11 18:37:23 +0300

    chore: setup project environment and routing

--- nya-train/src/App.jsx
+++ nya-train/src/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import Home from './pages/Home';
+import Booking from './pages/Booking';
+import Success from './pages/Success';
+import MyTickets from './pages/MyTickets';
 import './index.css';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/booking/:trainId" element={<Booking />} />
+        <Route path="/success" element={<Success />} />
+        <Route path="/my-tickets" element={<MyTickets />} />
+      </Routes>
+      <ToastContainer position="bottom-right" autoClose={3000} />
     </div>
   );
 }
